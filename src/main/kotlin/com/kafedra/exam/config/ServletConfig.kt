0ce48b210f58d6788dfb8f25de325ee7142419e5
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 11:21:53 +0700

    Added ServletConfig

---
+++ src/main/kotlin/com/kafedra/exam/config/ServletConfig.kt
@@ -0,0 +1,15 @@
+package com.kafedra.exam.config
+
+import com.google.inject.Guice
+import com.google.inject.Injector
+import com.google.inject.servlet.GuiceServletContextListener
+import com.google.inject.servlet.ServletModule
+
+class ServletConfig : GuiceServletContextListener() {
+    override fun getInjector(): Injector = Guice.createInjector(object : ServletModule() {
+        override fun configureServlets() {
+            super.configureServlets()
+
+        }
+    })
+}

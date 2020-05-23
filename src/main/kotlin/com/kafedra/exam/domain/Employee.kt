99aa21f0090ac16eda5ee8172030a15894109b28
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 14:53:04 +0700

    Changed domain classes

--- src/main/kotlin/com/kafedra/exam/domain/Employee.kt
+++ src/main/kotlin/com/kafedra/exam/domain/Employee.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.Expose
 class Employee {
     @Expose var id = 0
     @Expose var name = ""
-    var department: Department? = null
+    @Expose var departmentId = 0 // Only for deserialization
     var version: Long = 0
 }
--- src/main/kotlin/com/kafedra/exam/domain/Number.kt
+++ src/main/kotlin/com/kafedra/exam/domain/Number.kt
@@ -7,6 +7,6 @@ class Number {
     @Expose var id = 0
     @Expose var type: PhoneType? = null
     @Expose var number = ""
-    var employee: Employee? = null
+    @Expose var employeeId = 0 // Only for deserialization
     var version: Long = 0
 }

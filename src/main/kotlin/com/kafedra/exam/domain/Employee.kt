4a52956dd2a7b990771bf3be07128d1dfc7bf91d
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 17:03:51 +0700

    Changed Employee

--- src/main/kotlin/com/kafedra/exam/domain/Employee.kt
+++ src/main/kotlin/com/kafedra/exam/domain/Employee.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.Expose
 class Employee {
     @Expose var id = 0
     @Expose var name = ""
-    @Expose var departmentId = 0 // Only for deserialization
+    @Expose var departmentId: Int? = null // Only for deserialization
     var version: Long = 0
 }

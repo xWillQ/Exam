42655006fc2aea30e24c055d20cdd0334cfa5505
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 13:01:39 +0700

    Updated migrations

--- src/main/resources/migrations/V1.1__Add_test_data.sql
+++ src/main/resources/migrations/V1.1__Add_test_data.sql
@@ -1,17 +1,17 @@
-INSERT INTO departments
+INSERT INTO departments(id, title, number)
 VALUES
     (1, 'sales', '123'),
     (2, 'it', '345'),
     (3, 'hr', '876'),
     (4, 't', '3456');
-INSERT INTO employees
+INSERT INTO employees(id, name, departmentId)
 VALUES
     (1, 'vasya', 1),
     (2, 'petya', 1),
     (3, 'masha', 3);
-INSERT INTO numbers
+INSERT INTO numbers(id, type, number, employeeId)
 VALUES
-    (1, 'internal_cell', '8-800-111-3456', 1),
-    (2, 'external_cell', '8-954-245-6543', 1),
-    (3, 'home', '2345667', 2),
-    (4, 'internal_cell', '8-254-386-367', 2);
+    (1, 'INTERNAL_CELL', '8-800-111-3456', 1),
+    (2, 'EXTERNAL_CELL', '8-954-245-6543', 1),
+    (3, 'HOME', '2345667', 2),
+    (4, 'INTERNAL_CELL', '8-254-386-367', 2);

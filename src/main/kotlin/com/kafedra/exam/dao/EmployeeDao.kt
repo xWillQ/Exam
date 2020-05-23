4e2f5bcdfed0de740adb909755fe8dcb3fd4ac6a
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 14:52:40 +0700

    Added ability to add employees and numbers

--- src/main/kotlin/com/kafedra/exam/dao/EmployeeDao.kt
+++ src/main/kotlin/com/kafedra/exam/dao/EmployeeDao.kt
@@ -2,6 +2,7 @@ package com.kafedra.exam.dao
 
 import com.google.inject.Inject
 import com.kafedra.aaapp.di.HibernateProvider
+import com.kafedra.exam.domain.Department
 import com.kafedra.exam.domain.Employee
 
 class EmployeeDao {
@@ -10,9 +11,19 @@ class EmployeeDao {
 
     fun getEmployeesByDepartment(depId: Int) : List<Employee> {
         val session = sessionProvider.get().openSession()
-        val query = session.createQuery("FROM Employee WHERE department.id = $depId", Employee::class.java)
+        val query = session.createQuery("FROM Employee WHERE departmentId = $depId", Employee::class.java)
         val employees = query.resultList
         session.close()
         return employees
+    }
+
+    fun addEmployee(emp: Employee) {
+        val session = sessionProvider.get().openSession()
+        session.beginTransaction()
+
+        session.save(emp)
+
+        session.transaction.commit()
+        session.close()
     }
 }

88832e03da5d3273cb7da3f2c4f83acc1007dfa6
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 11:25:23 +0700

    Binded /hello to HelloServlet

--- src/main/kotlin/com/kafedra/exam/config/ServletConfig.kt
+++ src/main/kotlin/com/kafedra/exam/config/ServletConfig.kt
@@ -4,11 +4,13 @@ import com.google.inject.Guice
 import com.google.inject.Injector
 import com.google.inject.servlet.GuiceServletContextListener
 import com.google.inject.servlet.ServletModule
+import com.kafedra.exam.servlet.HelloServlet
 
 class ServletConfig : GuiceServletContextListener() {
     override fun getInjector(): Injector = Guice.createInjector(object : ServletModule() {
         override fun configureServlets() {
             super.configureServlets()
+            serve("/hello").with(HelloServlet::class.java)
 
         }
     })

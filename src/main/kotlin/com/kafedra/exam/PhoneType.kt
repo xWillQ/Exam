90a83ae8b8508d970b66a32f9cee24fe0772ccae
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-23 12:24:04 +0700

    Added PhoneType enum

---
+++ src/main/kotlin/com/kafedra/exam/PhoneType.kt
@@ -0,0 +1,8 @@
+package com.kafedra.exam
+
+enum class PhoneType(val type: String) {
+    INTERNAL_CELL("internal_cell"),
+    EXTERNAL_CELL("external_cell"),
+    HOME("home"),
+    EMERGENCY("emergency")
+}
